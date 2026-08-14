3a5e0f53432fb4c42b9d53608c908af2878ca943
Anton Makarov <makarov_13@mail.ru>
2026-08-14 16:05:09 +0300

    Bump version to 0.5.1

--- cmake/IBStartVersion.cmake
+++ cmake/IBStartVersion.cmake
@@ -1,3 +1,3 @@
 # Single source of truth for the IBStart product version.
 # Use SemVer without build metadata, for example: 0.5.0 or 1.0.0.
-set(IBSTART_VERSION "0.5.0")
+set(IBSTART_VERSION "0.5.1")
