72dffb140c241623398adf7305563809e510c93a
Anton Makarov <makarov_13@mail.ru>
2026-08-14 15:48:03 +0300

    Release version 0.5.0

--- cmake/IBStartVersion.cmake
+++ cmake/IBStartVersion.cmake
@@ -1,3 +1,3 @@
 # Single source of truth for the IBStart product version.
-# Use SemVer without build metadata, for example: 0.1.0-alpha.2 or 1.0.0.
-set(IBSTART_VERSION "0.1.0-alpha.1")
+# Use SemVer without build metadata, for example: 0.5.0 or 1.0.0.
+set(IBSTART_VERSION "0.5.0")
