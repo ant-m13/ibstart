ff9f029db82a2d5f42969e8fba09db4edab99d21
Anton Makarov <makarov_13@mail.ru>
2026-08-24 09:35:32 +0300

    chore(release): prepare version 0.7.0

--- cmake/IBStartVersion.cmake
+++ cmake/IBStartVersion.cmake
@@ -1,3 +1,3 @@
 # Single source of truth for the IBStart product version.
 # Use SemVer without build metadata, for example: 0.6.0 or 1.0.0.
-set(IBSTART_VERSION "0.6.0")
+set(IBSTART_VERSION "0.7.0")
