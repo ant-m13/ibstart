185acd9b85e3ebce2beeecd3ec9c1a876ac68ebe
Anton Makarov <makarov_13@mail.ru>
2026-08-26 10:00:26 +0300

    chore(release): prepare version 0.7.4

--- cmake/IBStartVersion.cmake
+++ cmake/IBStartVersion.cmake
@@ -1,6 +1,6 @@
 # Single source of truth for the IBStart product version.
 # Use SemVer without build metadata, for example: 0.6.0 or 1.0.0.
-set(IBSTART_VERSION "0.7.3")
+set(IBSTART_VERSION "0.7.4")
 
 # GitHub repository used by the built-in update check.
 set(IBSTART_GITHUB_OWNER "ant-m13")
